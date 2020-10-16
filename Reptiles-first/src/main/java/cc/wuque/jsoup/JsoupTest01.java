ee148f0bf339e10fbe03da62fa71ba7ad6ed23fe
wuque <wuquejs@163.com>
2020-10-16 23:49:28 +0800

    使用Jsoup解析网站网站标签内容

--- Reptiles-first/src/main/java/cc/wuque/jsoup/JsoupTest01.java
+++ Reptiles-first/src/main/java/cc/wuque/jsoup/JsoupTest01.java
@@ -8,7 +8,7 @@ import java.net.URL;
 /**
  *使用Jsoup解析网站网站标签内容
  */
-public class JsoupFirstTest {
+public class JsoupTest01 {
     public static void main(String[] args) throws Exception{
         //解析url地址
         //第一个参数是访问url，第二个参数是访问时候的超时时间
